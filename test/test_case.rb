e8ca0074b2914e5546e1bf3602bde4c5715e6c56
Tim <tim@maplejar.co.uk>
2015-03-19 12:57:07 +0000

    test_helper and test_case boiler plate

---
+++ test/test_case.rb
@@ -0,0 +1,4 @@
+require 'test/unit'
+
+class TestCase < Test::Unit::TestCase
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,7 @@
+require 'test/unit'
+require 'mocha/setup'
+require 'debugger'
+require 'tmpdir'
+require 'benchmark'
+
+require_relative "./test_case"
